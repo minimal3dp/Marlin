852c122a479760e2ac90929242d24d6f8667aa61
Micheal J. Wilson <wilsongis@gmail.com>
2022-09-04 19:50:40 -0500

    Fixed Fan

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(M3DP, E5P-SKR3 2.1.5)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(M3DP, E5P-SKR3 2.1.6)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -87,7 +87,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_V3_0
+  #define MOTHERBOARD BOARD_BTT_SKR_V3_0 //BOARD_BTT_SKR_V3_0_EZ
 #endif
 
 /**
@@ -135,7 +135,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "E5P-SKR3 2.1.5"
+#define CUSTOM_MACHINE_NAME "E5P-SKR3 2.1.6"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
