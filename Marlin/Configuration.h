4819b7638c2c95f533fee9f8fa3d1d1ce60b0b87
Mike Wilson <wilsongis@gmail.com>
2023-01-08 18:09:20 -0600

    Updated to current malin bugfix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -136,7 +136,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3_M3D2.1.0"
+#define CUSTOM_MACHINE_NAME "Ender-3_M3D2.1.002"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
